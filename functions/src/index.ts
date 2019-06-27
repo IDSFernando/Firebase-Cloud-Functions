6f06026c95f961cfe83a57d1dc174f68ace4c655
Fernando <fernando@MacBook-Pro-de-Fernando.local>
2019-06-27 17:42:11 -0500

    Terminado

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,28 @@
-import * as functions from 'firebase-functions';
+import * as functions from 'firebase-functions'
+import * as admin from 'firebase-admin'
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
 
-export const getName = functions.https.onRequest((request, response) => {
-    response.send(`name = ${request.query.name}`)
-})
+admin.initializeApp()
+
+const path = require('path')
+const vision = require('@google-cloud/vision')
+const visionClient = new vision.ImageAnnotatorClient()
+
+// Dedicated bucket for cloud function invocation
+const bucketName = 'cloud-functions-soa.appspot.com'
+
+export const imageTrigger = functions.storage.bucket(bucketName).object().onFinalize(
+    async (object) => {
+        console.log(object)
+        const filePath = object.name
+        const fileName = path.basename(filePath)
+        const fileUrl = `gs://${bucketName}/${filePath}`
+        const fileID = fileName.split('.jpg')[0]
+        const docRef = admin.firestore().collection('photos').doc(fileID)
+        const results = await visionClient.labelDetection(fileUrl)
+        const labels = results[0].labelAnnotations.map((obj:any) => obj.description)
+        return docRef.set(
+            {labels}
+        )
+    }
+)
